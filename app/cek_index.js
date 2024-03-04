5dad7760a2fda5adb9f977b8eda66092db928176
piumenotechnology <intelitech@piumeno.co>
2024-03-05 02:51:47 +0800

    new commit

---
+++ app/cek_index.js
@@ -0,0 +1,17 @@
+// import 'dotenv/config';
+// import express, { json }  from 'express';
+// import reqLog from './middleware/middleware.js'
+// import router from './routes/ai-routes.js';
+
+// const app = express();
+
+// //middleware
+// app.use (reqLog);
+// app.use (json());
+
+// app.use('/api/v1', router);
+
+// export default app;
+// //run server
+
+
--- server.js
+++
@@ -1,7 +0,0 @@
-import app from './app/index.js'
-
-const PORT = process.env.PORT || 8000;
-
-app.listen (PORT, () => {
-    console.log(`server running on port ${PORT}`)
-})
