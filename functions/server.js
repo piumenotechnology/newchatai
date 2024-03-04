336ac920bcc1f7369f3b93cfaee679bc7a22169d
piumenotechnology <153091153+piumenotechnology@users.noreply.github.com>
2024-03-05 04:24:40 +0800

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -1,1 +1,17 @@
+import 'dotenv/config';
+import express, { json }  from 'express';
+import reqLog from './app/middleware/middleware.js'
+import router from './app/routes/ai-routes.js';
 
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+//middleware
+app.use (reqLog);
+app.use (json());
+
+app.use('/api/v1', router);
+
+app.listen (PORT, () => {
+    console.log(`server running on port ${PORT}`)
+})
